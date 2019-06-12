ecbc78085966bc9bd416d377e435a92d8bfa356a
Alejandro <aletea@bas.ac.uk>
2019-06-12 12:08:00 +0200

    Added first version of WANG's impulse noise algorithm.

--- echopy/mask_impulse.py
+++ echopy/mask_impulse.py
@@ -7,7 +7,10 @@ Created on Fri Apr 27 14:24:44 2018
 """
 
 import numpy as np
-from echopy.operations import binv, binvback
+from echopy.operations import binv, binvback, tolin, tolog
+from skimage.morphology import erosion
+from skimage.morphology import dilation
+from scipy.ndimage.filters import median_filter as medianf
 
 def ryan(Sv, r, m=5, n=1, thr=10, start=0):
     """
@@ -59,7 +62,7 @@ def ryan_iterable(Sv, r, m=5, n=(1,2), thr=10, start=0):
     Modified from "ryan" so that the parameter "n" can be provided multiple
     times. It enables the algorithm to iterate and perform comparisons at
     different n distances. Resulting masks at each iteration are combined in
-    a single mask. By setting multiple n distances the algorith can detect 
+    a single mask. By setting multiple n distances the algorithm can detect 
     spikes adjacent each other.
     
     Args:
@@ -99,6 +102,64 @@ def ryan_iterable(Sv, r, m=5, n=(1,2), thr=10, start=0):
     
     return [mask[:, start:], mask_[:, start:]]
 
+
+def wang(Sv, thr=(-70,-40),
+         erode=[(3,3)], dilate=[(5,5),(7,7)], median=[(7,7)]):
+    """
+    Clean impulse noise from Sv data following the method decribed by:
+        
+        Wang et al. (2015) ’A noise removal algorithm for acoustic data with
+        strong interference based on post-processing techniques’, CCAMLR
+        SG-ASAM: 15/02.
+        
+    This algorithm runs different cycles of erosion, dilation, and median
+    filtering to clean up Sv from impulse noise. Note that this function
+    returns a clean/corrected Sv array, instead of a boolean array indicating
+    the occurrence of impulse noise.
+        
+    Args:
+        Sv (float)  : 2D numpy array with Sv data (dB).
+        thr (int)   : 2-elements tupple with bottom and top Sv thresholds (dB).
+        erode (int) : list of 2-element tupples indicating the window's size
+                      for each erosion cycle.
+        dilate (int): list of 2-element tupples indicating the window's size
+                      for each dilation cycle.
+        median (int): list of 2-element tupples indicating the window's size
+                      for each median filter cycle.
+                      
+    Returns:
+        float: 2D numpy array with clean Sv data.
+    """
+    
+    # set values out of threshold to -999
+    Sv_thresholded = Sv.copy()
+    Sv_thresholded[(Sv<thr[0])|(Sv>thr[1])] = -999
+    
+    # run erosion cycles
+    Sv_eroded = Sv_thresholded.copy()
+    for e in erode:
+        Sv_eroded = erosion(Sv_eroded, np.ones(e))
+
+    # run dilation cycles
+    Sv_dilated = Sv_eroded.copy()
+    for d in dilate:
+        Sv_dilated = dilation(Sv_dilated, np.ones(d))
+    
+    # non-vacant values after erosion/dilation are corrected to corresponding
+    # values before erosion/dilation
+    Sv_corrected1 = Sv_dilated.copy()
+    Sv_corrected1[Sv_dilated!=-999] = Sv_thresholded[Sv_dilated!=-999]
+    
+    # vacant values after erosion/dilation are corrected to median-filtered
+    # values before erosion/dilation
+    Sv_median = Sv_thresholded
+    for m in median:
+        Sv_median = tolog(medianf(tolin(Sv_median), footprint=np.ones(m)))    
+    Sv_corrected2 = Sv_corrected1.copy()
+    Sv_corrected2[Sv_dilated==-999] = Sv_median[Sv_dilated==-999]
+    
+    return Sv_corrected2
+
 def other():
     """
     Note to contributors:
@@ -108,3 +169,4 @@ def other():
         
         Please, check /DESIGN.md to adhere to our coding style.
     """
+    
