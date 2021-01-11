5ed4fba981fb624f5158762c5cdeed95beab4c56
alejandro-ariza <alejandro.ariza@ird.fr>
2021-01-11 18:28:25 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "echopy",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "EchoPY",
     author_email = "echopy@protonmail.com",
     description= "Fisheries acoustic processing in Python",
