6d96fd206f99926fc06b0cb5be91820bb7a2ff37
alejandro-ariza <alejandro.ariza@ird.fr>
2023-01-27 11:06:16 +0100

    added example script to classify acoustic seascapes

---
+++ tests/classify_acoustic_seascape.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Example script to classify acoustic seascape using functional data analysis.
+"""
+
+__authors__ = ['Alejandro Ariza',
+               'Jérémie Habasque',
+               'Etienne Pauthenet'
+               ] 
+
